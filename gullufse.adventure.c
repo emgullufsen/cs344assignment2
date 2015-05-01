76c5b52d67eeaa1be462595d09c82ee9468f3299
emgullufsen <ericgullufsen@gmail.com>
2015-05-01 10:16:18 -0700

    wee

--- gullufse.adventure.c
+++ gullufse.adventure.c
@@ -4,3 +4,27 @@
 //
 
 #include<stdio.h>
+
+// room types
+
+const char *START = "START_ROOM";
+
+const char *MID = "MID_ROOM";
+
+const char *END = "END_ROOM";
+
+// room names
+
+const char *ROOM_NAMES[10] = {"Tsar Peters Docha","King Henrys Oubliette","Stalins SMERSH Interrogation Room","Bonapartes Foyer","Tutankhamuns Crypt","T. Roosevelts Rifle Range","Trumans Bomb Bay","Churchills Brandy Basement","Agamemnons Beach Tent", "Neros Wine Cellar"};
+
+//create directory gullufse.rooms.<PID>
+
+//create seven room files
+
+//create 7 room structs and write to files
+
+struct roomfile
+{
+	int name; // index in ROOM_NAMES
+	int connections[6]; //indices in ROOM
+};
